9fe1208b96ee782ac72ea75182d7d53bc50aba51
Vikki415 <vjongetjes@gmail.com>
2016-03-08 17:16:39 -0500

    Added files via upload

---
+++ newapplicationform.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Author: Vikki
+Create Date: March 8, 2016
+Description: This is the form to collect data about a new application
+*/
+?>
+ <html>
+ <style>
+table.db-table 		{ border-right:1px solid #ccc; border-bottom:1px solid #ccc; }
+table.db-table th	{ background:#eee; padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; }
+table.db-table td	{ padding:5px; border-left:1px solid #ccc; border-top:1px solid #ccc; }
+</style>
+ <head>
+	<title>Application input form </title>
+ </head>
+<body>
+
+<form>
+  Company Name:<br>
+  <input type="text" name="Company"><br>
+  
+</form>
+
+<?php
+
+
+	
+
+
+?>
+</body>
+</html>
---
+++ saveapp.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Author: Vikki
+Create Date: March 8, 2016
+Description: confirms to user that the submission is successful
+*/
+?>
+<?php
+
+include "/functions.php"
+
+?>
+<?php
+
+echo "Thank you for your submission."
+
+?>
